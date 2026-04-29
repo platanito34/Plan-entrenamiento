5ce8c5c1f559c3b269be2641d7f2bc057c75e76a
platanito34 <leogarcimul@gmail.com>
2026-04-29 12:57:21 +0200

    parche backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(helmet());
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ironlog-v1';
+const CACHE_NAME = 'ironlog-v2';
 
 const PRECACHE_URLS = [
   '/app.html',
